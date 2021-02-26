76cab415f0da30a3fd580d599001ffba78861423
Cyrus Behroozi <cyrus.behr@gmail.com>
2021-02-25 19:57:14 -0800

    Fixed build instructions for mxnet

--- 3rd_party_libs/build_mxnet.sh
+++ 3rd_party_libs/build_mxnet.sh
@@ -3,6 +3,7 @@ test -e incubator-mxnet || git clone --recursive https://github.com/apache/incub
 
 cd incubator-mxnet
 git reset --hard 2fc0706 # 1.8.0.rc3
+git submodule update --init --recursive
 
 mkdir build_amd64
 cd build_amd64
