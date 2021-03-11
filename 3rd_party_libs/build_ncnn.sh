9a3717953256f7338a913cd63275508d8fd0a8bc
Cyrus Behroozi <cyrus.behr@gmail.com>
2021-03-11 13:55:05 -0800

    Fixed ncnn build script

--- 3rd_party_libs/build_ncnn.sh
+++ 3rd_party_libs/build_ncnn.sh
@@ -7,9 +7,9 @@ git reset --hard 5e4ea0b # Jan 24 release
 mkdir build_amd64
 cd build_amd64
 
+cmake -D NCNN_BUILD_EXAMPLES=OFF -D NCNN_VULKAN=OFF -D NCNN_AVX2=ON -D CMAKE_BUILD_TYPE=Release ..
 
+nproc | xargs -I % make -j%
 
-#nproc | xargs -I % make -j%
-#
-#make install
-#
+make install
+
